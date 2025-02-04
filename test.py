327ebfb82e9b7d0266175b0ea0f78e4243d6999e
Benjamín Aguilera <benjamin.aguilera64@gmail.com>
2025-02-04 15:22:59 -0300

    add sheet copy, change sheet structure, ui tweaks

--- test.py
+++ test.py
@@ -1,36 +1,139 @@
 import openpyxl, re
 import tkinter as tk
-from openpyxl.styles import Font, Alignment
+from openpyxl.styles import Font, Alignment, PatternFill
 from tkinter import ttk
 from tkinter import filedialog as fd
-# from tkinter.filedialog import asksaveasfile
 from tkinter.messagebox import showinfo
-from tkinter import StringVar
-from tkinter import simpledialog
+from copy import copy
 import os
 
-
-# fd.ask
+import openpyxl.styles
 
 DEFAULT_DIR = "."
 
-# inputPath = StringVar()
 
-def XLSXHandling(inFilename, outFilename):
-    # Abrir archivo excel de entrada
+def activator():
+    if check.get() == 1:
+        importButton.config(state="normal")
+    else:
+        importButton.config(state="disabled")
+
+
+def copySheet(sourceSheet, targetSheet):
+    copyCells(sourceSheet, targetSheet)
+    copySheetAttributes(sourceSheet, targetSheet)
+
+
+# Función extraída de StackOverflow https://stackoverflow.com/a/68800310
+def copyCells(sourceSheet, targetSheet):
+    for (row, col), sourceCell in sourceSheet._cells.items():
+        targetCell = targetSheet.cell(column=col, row=row)
+
+        targetCell._value = sourceCell._value
+        targetCell.data_type = sourceCell.data_type
+
+        if sourceCell.has_style:
+            targetCell.font = copy(sourceCell.font)
+            targetCell.border = copy(sourceCell.border)
+            targetCell.fill = copy(sourceCell.fill)
+            targetCell.number_format = copy(sourceCell.number_format)
+            targetCell.protection = copy(sourceCell.protection)
+            targetCell.alignment = copy(sourceCell.alignment)
+
+        if sourceCell.hyperlink:
+            targetCell._hyperlink = copy(sourceCell.hyperlink)
+
+        if sourceCell.comment:
+            targetCell.comment = copy(sourceCell.comment)
+            
+
+# Función extraída de StackOverflow: https://stackoverflow.com/a/68800310
+def copySheetAttributes(sourceSheet, targetSheet):
+    targetSheet.sheet_format = copy(sourceSheet.sheet_format)
+    targetSheet.sheet_properties = copy(sourceSheet.sheet_properties)
+    targetSheet.merged_cells = copy(sourceSheet.merged_cells)
+    targetSheet.page_margins = copy(sourceSheet.page_margins)
+    targetSheet.freeze_panes = copy(sourceSheet.freeze_panes)
+
+    # set row dimensions
+    # So you cannot copy the row_dimensions attribute. Does not work (because of meta data in the attribute I think). So we copy every row's row_dimensions. That seems to work.
+    for rn in range(len(sourceSheet.row_dimensions)):
+        targetSheet.row_dimensions[rn] = copy(sourceSheet.row_dimensions[rn])
+
+    if sourceSheet.sheet_format.defaultColWidth is None:
+        print('Unable to copy default column wide')
+    else:
+        targetSheet.sheet_format.defaultColWidth = copy(sourceSheet.sheet_format.defaultColWidth)
+
+    # set specific column width and hidden property
+    # we cannot copy the entire column_dimensions attribute so we copy selected attributes
+    for key, value in sourceSheet.column_dimensions.items():
+        targetSheet.column_dimensions[key].min = copy(sourceSheet.column_dimensions[key].min)   # Excel actually groups multiple columns under 1 key. Use the min max attribute to also group the columns in the targetSheet
+        targetSheet.column_dimensions[key].max = copy(sourceSheet.column_dimensions[key].max)  # https://stackoverflow.com/questions/36417278/openpyxl-can-not-read-consecutive-hidden-columns discussed the issue. Note that this is also the case for the width, not onl;y the hidden property
+        targetSheet.column_dimensions[key].width = copy(sourceSheet.column_dimensions[key].width) # set width for every column
+        targetSheet.column_dimensions[key].hidden = copy(sourceSheet.column_dimensions[key].hidden)
+
+
+def XLSXHandling(ticketsFilename, inFilename, outFilename):
+
     workbook = openpyxl.load_workbook(filename=inFilename)
+    if (ticketsFilename != ""):
+        targetSheet = workbook.create_sheet("Tickets")
+        tickWorkbook = openpyxl.load_workbook(filename=ticketsFilename)
+        sourceSheet = tickWorkbook["Sheet 1"]
+        copySheet(sourceSheet, targetSheet)
+
+        if "Sheet" in workbook.sheetnames:
+            workbook.remove(workbook["Sheet"])
+
+    # Abrir archivo excel de entrada
     ticketsSheet = workbook["Tickets"]
-    tickMax = ticketsSheet.max_row
+    tickMax = ticketsSheet.max_row # Número de fila del último ticket registrado en la hoja de tickets
     print(tickMax)
     sheet = workbook["Item 12"] # Selección de hoja
-    lastTicket = sheet.max_row - 3
-    print(lastTicket)
+    lastTicket = sheet.max_row - 3 # Número de fila del último ticket de la hoja original
 
     contents = []
 
     # Guarda contenidos de hoja 
     for row in sheet.iter_rows():
         contents.append(row)
+
+
+    cyan = openpyxl.styles.colors.Color(rgb="0000b0F0")
+    headerFont = Font(name="Calibri", size=9, bold=True, color="00FFFFFF")
+    headerFill = PatternFill(start_color=cyan, end_color=cyan, fill_type="solid")
+
+    sheet["J1"] = "Apertura"
+    sheet["J1"].font = headerFont
+    sheet["J1"].fill = headerFill
+
+    sheet["K1"] = "Resuelto"
+    sheet["K1"].font = headerFont
+    sheet["K1"].fill = headerFill
+
+    sheet["L1"] = "Cerrado"
+    sheet["L1"].font = headerFont
+    sheet["L1"].fill = headerFill
+
+    sheet["M1"] = "Mejor fecha"
+    sheet["M1"].font = headerFont
+    sheet["M1"].fill = headerFill
+
+    sheet["N1"] = "Tiempo resolución"
+    sheet["N1"].font = headerFont
+    sheet["N1"].fill = headerFill
+
+    sheet["O1"] = "Tiempo cierre"
+    sheet["O1"].font = headerFont
+    sheet["O1"].fill = headerFill
+
+    sheet["P1"] = "HH Tiempo NC"
+    sheet["P1"].font = headerFont
+    sheet["P1"].fill = headerFill    
+
+
+
 
     i = 1
     addedCells = 0
@@ -45,11 +148,12 @@ def XLSXHandling(inFilename, outFilename):
         # Si ATM tiene por lo menos un ticket
         if len(numList) > 0:
             sheet[f"C{i}"] = "2024 " + numList[0] # Reescribe número de ticket con formato correcto
-            sheet[f"K{i}"] = f'=IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE)), "No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE))'
-            sheet[f"M{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)))'
-            sheet[f"N{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)))'
-            sheet[f"O{i}"] = f'=IF(ISERR(M{i}-K{i}), "No Disponible", M{i}-K{i})'
-            sheet[f"P{i}"] = f'=IF(ISERR(N{i}-K{i}), "No Disponible", N{i}-K{i})'
+            sheet[f"J{i}"] = f'=IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE)), "No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE))'
+            sheet[f"K{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)))'
+            sheet[f"L{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)))'
+            sheet[f"N{i}"] = f'=IF(ISERR(K{i}-J{i}), "No Disponible", K{i}-J{i})'
+            sheet[f"O{i}"] = f'=IF(ISERR(L{i}-J{i}), "No Disponible", L{i}-J{i})'
+            sheet[f"P{i}"] = f'=IF(ISTEXT(N{i}),IF(ISTEXT(O{i}),"No disponible",N{i}),IF(ISTEXT(O{i}),N{i},MIN(N{i},O{i})))'
 
             # Si ATM tiene más de un ticket
             if len(numList) > 1:
@@ -67,21 +171,23 @@ def XLSXHandling(inFilename, outFilename):
                     sheet[f"A{i}"] = ATM
                     sheet[f"B{i}"] = Comuna
                     sheet[f"C{i}"] = newNum
-                    sheet[f"K{i}"] = f'=IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE)), "No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE))'
-                    sheet[f"M{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)))'
-                    sheet[f"N{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)))'
-                    sheet[f"O{i}"] = f'=IF(ISERR(M{i}-K{i}), "No Disponible", M{i}-K{i})'
-                    sheet[f"P{i}"] = f'=IF(ISERR(N{i}-K{i}), "No Disponible", N{i}-K{i})'
+                    sheet[f"J{i}"] = f'=IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE)), "No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},22,FALSE))'
+                    sheet[f"K{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},23,FALSE)))'
+                    sheet[f"L{i}"] = f'=IF(ISBLANK(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"Vacío",IF(ISNA(VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)),"No Encontrado", VLOOKUP(C{i},Tickets!$A$2:$X${tickMax},24,FALSE)))'
+                    sheet[f"N{i}"] = f'=IF(ISERR(K{i}-J{i}), "No Disponible", K{i}-J{i})'
+                    sheet[f"O{i}"] = f'=IF(ISERR(L{i}-J{i}), "No Disponible", L{i}-J{i})'
+                    sheet[f"P{i}"] = f'=IF(ISTEXT(N{i}),IF(ISTEXT(O{i}),"No disponible",N{i}),IF(ISTEXT(O{i}),N{i},MIN(N{i},O{i})))'
+
                     
                     print(ATM, "|", Comuna, "|", newNum, "|", Apertura)
 
         # ATM sin número de ticket
         elif(len(numList) == 0 and i != 1):
+            sheet[f"J{i}"] = "No Disponible"
             sheet[f"K{i}"] = "No Disponible"
-            sheet[f"M{i}"] = "No Disponible"
+            sheet[f"L{i}"] = "No Disponible"
             sheet[f"N{i}"] = "No Disponible"
             sheet[f"O{i}"] = "No Disponible"
-            sheet[f"P{i}"] = "No Disponible"
 
 
         i += 1
@@ -96,21 +202,21 @@ def XLSXHandling(inFilename, outFilename):
             cell.alignment = Alignment(horizontal = "center", vertical = "center")
 
     # Aplica formato de fecha a columnas correspondientes
-    for r in sheet[ f"J1:N{lastTicket + addedCells + 3}" ]:
+    for r in sheet[ f"J1:M{lastTicket + addedCells + 3}" ]:
         for cell in r:
             cell.number_format = "dd/mm/yyyy h:mm"
 
     # Aplica formato de hora a columnas correspondientes
-    for r in sheet[ f"O1:P{lastTicket + addedCells + 3}" ]:
+    for r in sheet[ f"N1:P{lastTicket + addedCells + 3}" ]:
         for cell in r:
-            cell.number_format = "h:mm:ss"
+            cell.number_format = "[h]:mm:ss"
 
     # Aplica ancho de columna
     sheet.column_dimensions["J"].width = 20
     sheet.column_dimensions["K"].width = 20
     sheet.column_dimensions["L"].width = 20
     sheet.column_dimensions["M"].width = 20
-    sheet.column_dimensions["N"].width = 20
+    sheet.column_dimensions["N"].width = 15
     sheet.column_dimensions["O"].width = 15
     sheet.column_dimensions["P"].width = 15
 
@@ -120,26 +226,40 @@ def XLSXHandling(inFilename, outFilename):
 
 
 # Ventana de selección de archivo
-def selectInputFile():
+def selectInputFile(inType):
 
-    global inputFilename
+    global inputFilename, ticketsFilename
     filetypes = (
         ('Excel spreadsheets', '*.xlsx'),
         ('All files', '*.*')
     )
 
-    inputFilename = fd.askopenfilename(
-        title = "Abrir archivo",
-        initialdir="/",
-        filetypes=filetypes
-    )
 
-    if inputFilename:
-        showinfo(title="Archivo seleccionado", message=inputFilename)
-        if hasattr(window, "label_infile"):
-            window.label_infile.config(text="Archivo de entrada: " + inputFilename)
-    else:
-        showinfo(title="Error", message="No se ha seleccionado ningún archivo")
+    if (inType == "input"):
+        inputFilename = fd.askopenfilename(
+            title = "Abrir archivo",
+            initialdir=DEFAULT_DIR,
+            filetypes=filetypes
+        )
+        if inputFilename:
+            showinfo(title="Archivo seleccionado", message=inputFilename)
+            if hasattr(window, "label_infile"):
+                window.label_infile.config(text="Archivo de entrada: " + inputFilename)
+        else:
+            showinfo(title="Error", message="No se ha seleccionado ningún archivo")
+    elif (inType == "tickets"):
+        ticketsFilename = fd.askopenfilename(
+            title = "Abrir archivo de tickets",
+            initialdir = DEFAULT_DIR,
+            filetypes = filetypes
+        )
+        if ticketsFilename:
+            showinfo(title = "Archivo de tickets seleccionado", message = ticketsFilename)
+            if hasattr(window, "label_tickfile"):
+                window.label_tickfile.config(text="Archivo de tickets: " + ticketsFilename)
+            else:
+                showinfo(title = "Error", message = "No se ha seleccionado un archivo de tickets")
+
         
     # inputPath.set(filename)
 
@@ -173,7 +293,7 @@ def saveAs() :
         showinfo(title="", message=outputFilename)
         if hasattr(window, "label_outfile"):
             window.label_outfile.config(text="Nombre de salida: " + outputFilename)
-        XLSXHandling(inputFilename, outputFilename)
+        XLSXHandling(ticketsFilename, inputFilename, outputFilename)
     else:
         showinfo(title="Error", message="No se ha seleccionado un nombre de salida")
 
@@ -184,26 +304,45 @@ def saveAs() :
 window = tk.Tk()
 window.resizable(False, False)
 window.title("AutoMultas")
-window.geometry("300x300")
+window.geometry("400x400")
 # frame = tk.Frame(master=window, width=300, height=250, border=2, background= "yellow").pack()
 
+ticketsFilename = ""
 inputFilename = ""
 outputFilename = ""
 
-window.label_infile = tk.Label(window, text="No se ha seleccionado un archivo de entrada")
+
+impFrame = tk.Frame(master=window, width=450, height=150, bd=4, relief=tk.RAISED)
+impFrame.pack(pady=5)
+
+check = tk.IntVar()
+importCheck = ttk.Checkbutton(impFrame, text="Importar tickets", variable=check, onvalue=1, offvalue=0, command=activator)
+importCheck.pack(pady=5)
+
+window.label_tickfile = tk.Label(impFrame, text="", wraplength=300)
+window.label_tickfile.pack()
+importButton = ttk.Button(master=impFrame, text="Abrir archivo de tickets", command= lambda: selectInputFile("tickets"), state="disabled")
+importButton.pack(padx=10, pady=5)
+
+
+openFrame = tk.Frame(master=window, width=450, height=100, bd=4, relief=tk.RAISED)
+openFrame.pack(pady=5)
+
+window.label_infile = tk.Label(openFrame, text="No se ha seleccionado un archivo de entrada", wraplength=300)
 window.label_infile.pack()
-openButton = ttk.Button(master=window, text="Abrir un archivo", command=selectInputFile)
+openButton = ttk.Button(master=openFrame, text="Abrir un archivo", command= lambda: selectInputFile("input"))
 openButton.pack(pady=10)
-# inputLabel = ttk.Label(master=window, text=inputFilename).pack()
 
-window.label_outfile = tk.Label(window, text="No se ha seleccionado un nombre de archivo de salida")
+# if check.get() == 1:
+#     openButton.config(state="normal")
+
+saveFrame = tk.Frame(master=window, width=450, height=100, bd=4, relief=tk.RAISED)
+saveFrame.pack()
+
+window.label_outfile = tk.Label(saveFrame, text="No se ha seleccionado un nombre de archivo de salida", wraplength=300)
 window.label_outfile.pack()
-saveButton = ttk.Button(master=window, text = "Guardar archivo", command=saveAs)
+saveButton = ttk.Button(master=saveFrame, text="Guardar archivo", command=saveAs)
 saveButton.pack(pady=10)
-# saveLabel = ttk.Label(master=window, text=outputFilename).pack()
-
-
-# XLSXHandling(inputFilename)
 
 
 window.mainloop()
